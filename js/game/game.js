507626273123200cbb5a05fb114206d83ebb2189
Mi-lex <mishin-lex@mail.ru>
2018-09-08 13:02:12 +0300

    Make game predownload images before displaying screen and setting timer

--- js/game/game.js
+++ js/game/game.js
@@ -1,7 +1,8 @@
 import GameType1View from "./game-view-1";
 import GameType2View from "./game-view-2";
 import GameType3View from "./game-view-3";
-import {changeView} from "../utilities";
+import {SpinningStar} from "../star.js";
+import {changeView, imageLoader} from "../utilities";
 import {initialState, getResult, setResult, tick} from "../data/game-data";
 import app from "../main";
 
@@ -12,15 +13,16 @@ const GameView = {
 };
 
 export default class GamePresenter {
-  constructor(levels, state = initialState) {
+  constructor(levels) {
     this.userName = localStorage.getItem(`name`);
     this.levels = levels;
-    this.state = state;
+    this.state = initialState;
     this.initialTime = this.state.time;
-    this.view = new GameView[this.getLevel().type](this.state, this.getLevel());
+    this.view = new GameView[this.level.type](this.state, this.level);
+    this.loadingScreen = new SpinningStar();
   }
 
-  getLevel() {
+  get level() {
     return this.levels[this.state.gameNumb - 1];
   }
 
@@ -39,12 +41,32 @@ export default class GamePresenter {
     }
   }
 
-  init() {
-    // Show current game screen and then start the timer
-    changeView(this.view);
-    this.startTimer();
+  imagePreLoader() {
+    const loadingPromises = [];
 
-    // Override view method which triggers when user gives answer
+    // Preloads pictures from current level
+    this.level.answers.map((answer) => {
+      const loader = imageLoader(answer.image.url).
+          then(() => {}, () => {
+            // In case of timeout or another error shows intro screen
+            this.view.onBack();
+          });
+      // Accumulates all pictures loadings in the array
+      loadingPromises.push(loader);
+    });
+
+    return Promise.all(loadingPromises);
+  }
+
+  init() {
+    changeView(this.loadingScreen);
+    this.imagePreLoader().then(() => {
+      // Show current game screen and then start the timer
+      changeView(this.view);
+      this.startTimer();
+    });
+
+    // While downloading pictures overrides view methods
 
     /**
      * Gets answer and considers next step depending on current state.
@@ -69,7 +91,7 @@ export default class GamePresenter {
         // if level is not last, make a new view depending on level type and execute init method recursively
       } else {
           this.state = setResult(this.state, result);
-          const nextLevel = this.getLevel();
+          const nextLevel = this.level;
           this.view = new GameView[nextLevel.type](this.state, nextLevel);
           this.init();
       }
@@ -77,6 +99,8 @@ export default class GamePresenter {
 
     this.view.onBack = () => {
       this.stopTimer();
+      this.state = initialState;
+      this.view = new GameView[this.level.type](this.state, this.level);
       app.showIntro();
     };
   }
